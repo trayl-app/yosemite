e7bfe506728a993b9b710c5b4a17f2b62c452aca
carlodenardin <64202858+carlodenardin@users.noreply.github.com>
2022-10-10 11:00:18 +0200

    Initial Commit

---
+++ yosemite/yosemiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  yosemiteApp.swift
+//  yosemite
+//
+//  Created by Carlo De Nardin on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct yosemiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
